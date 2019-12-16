704dfe2830720b11cba90f855f06e119b897112d
Chris Soliday <solidaycl@gmail.com>
2019-12-16 18:26:30 -0500

    Completed day1.

---
+++ day1/day1.py
@@ -0,0 +1,29 @@
+import math
+
+#
+# part 1
+#
+f = open("input", "r")
+
+sum = 0
+for x in f:
+    sum += int((int(x)/3)-2)
+print(sum)
+
+f.close()
+
+
+#
+# part 2
+#
+f = open("input", "r")
+
+sum = 0
+for x in f:
+    fuel = int((int(x)/3)-2)
+    while(fuel > 0):
+        sum+= fuel
+        fuel = int((int(fuel)/3)-2)
+print(sum)
+
+f.close()
