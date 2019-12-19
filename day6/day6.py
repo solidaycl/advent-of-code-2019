c204e53ab7b6da02c5d8eacf450739f34572a298
Chris Soliday <solidaycl@gmail.com>
2019-12-18 20:50:12 -0500

    Completed day6.

--- day6/day6.py
+++ day6/day6.py
@@ -1,25 +1,63 @@
 #
 # part 1
 #
-#f = open("input", "r")
-f = open("input2", "r")
+f = open("input", "r")
 
-adjacency = {}
+adjacencyForward = {}
 
 for x in f:
     conn = x.strip().split(")")
 
-    if(adjacency.get(conn[0]) != None):
-        adjacency.update({conn[0] : adjacency.get(conn[0]) + "|" + conn[1]})
+    if(adjacencyForward.get(conn[0]) != None):
+        adjacencyForward.update({conn[0] : adjacencyForward.get(conn[0]) + "|" + conn[1]})
     else:
-        adjacency.update({conn[0] : conn[1]});
+        adjacencyForward.update({conn[0] : conn[1]});
 
-def recCount(start, count):
-    if(adjacency.get(start) == None):
-        return count;
-    sum = 0
-    for x in list(adjacency.get(start).split("|")):
-        sum += count + recCount(x, count+1)
+def recTraverse(start, depth):
+    if(adjacencyForward.get(start) == None):
+        return depth;
+
+    sum = depth
+    for x in list(adjacencyForward.get(start).split("|")):
+        sum += recTraverse(x, depth+1)
     return sum
 
-print(str(recCount(adjacency.get("COM"), 0)))
+print(str(recTraverse("COM", 0)))
+
+f.close()
+
+
+#
+# part 2
+#
+f = open("input", "r")
+
+adjacencyBackward = {}
+
+for x in f:
+    conn = x.strip().split(")")
+
+    if(adjacencyBackward.get(conn[1]) != None):
+        adjacencyBackward.update({conn[1] : adjacencyBackward.get(conn[1]) + "|" + conn[0]})
+    else:
+        adjacencyBackward.update({conn[1] : conn[0]});
+
+def findSanta(lastPlanet, currentPlanet, depth):
+    if(currentPlanet == "SAN"):
+        return depth;
+
+    sum = 0
+    if(adjacencyForward.get(currentPlanet) != None):
+        for x in list(adjacencyForward.get(currentPlanet).split("|")):
+            if(x != lastPlanet):
+                sum += findSanta(currentPlanet, x, depth+1)
+
+    if(adjacencyBackward.get(currentPlanet) != None):
+        for x in list(adjacencyBackward.get(currentPlanet).split("|")):
+            if(x != lastPlanet):
+                sum += findSanta(currentPlanet, x, depth+1)
+    return sum;
+    
+print(str(findSanta(None, "YOU", 0)-2))
+
+f.close()
